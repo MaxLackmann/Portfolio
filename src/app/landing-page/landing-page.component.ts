1481eba674cd4aeb14d28867eb89cd49e3f8e096
MaxLackmann <lackmann_max@hotmail.com>
2024-09-16 10:13:55 +0200

    save

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { WelcomeAreaComponent } from '../welcome-area/welcome-area.component';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
 import { SkillsComponent } from '../skills/skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [WelcomeAreaComponent, AboutmeComponent, SkillsComponent],
+  imports: [WelcomeAreaComponent, AboutmeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  projects: { projectName: string; skills: string[] }[] = [
+    {
+      projectName: 'Join',
+      skills: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+    },
+    { projectName: 'El Pollo Loco', skills: ['HTML',  'CSS', 'JavaScript'] },
+    { projectName: 'Simple CRM', skills: ['Angular',  'Firebase',  'TypeScript' ] },
+  ];
 }
